e8a26b79bf83446b1eefa631aa79fc817ea6d565
suniltalukdar <suniltalukdar82@gmail.com>
2021-09-28 17:05:40 +0530

    Update DemoMongoApplication.java
    Added comment.

--- src/main/java/com/example/demo/mongo/DemoMongoApplication.java
+++ src/main/java/com/example/demo/mongo/DemoMongoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoMongoApplication {
 
 	public static void main(String[] args) {
+		//Fetch all the beans.
 		SpringApplication.run(DemoMongoApplication.class, args);
 	}
 
