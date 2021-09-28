2fdf9bfc2dd4760ba6bfa2dac129177326267381
suniltalukdar <suniltalukdar82@gmail.com>
2021-09-28 17:09:14 +0530

    Intermediate changes.

--- src/main/java/com/example/demo/mongo/DemoMongoApplication.java
+++ src/main/java/com/example/demo/mongo/DemoMongoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo.mongo;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoMongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoMongoApplication.class, args);
+		ApplicationContext ctx = (ApplicationContext) SpringApplication.run(DemoMongoApplication.class, args);
 	}
 
 }
